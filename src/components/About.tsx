4592e8443ed62093a120aa230ac60c53f26fecba
nathandelcid <nade8804@colorado.edu>
2024-11-26 20:42:14 -0600

    list group component

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+      <>
+        <h1>About Me</h1>
+        <p>Hi, I'm Nathan Delcid. I created this Binomial Options Pricing Model calculator to...</p>
+        {/* Add whatever content you want */}
+      </>
+    );
+  }
+  
+  export default About;
